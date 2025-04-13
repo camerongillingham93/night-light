8635f54d2150581f52bced1c81fbf99e65101171
Cameron Gillingham <144290859+camerongillingham93@users.noreply.github.com>
2025-04-13 13:43:42 -0700

    hello

--- software/nightlight-firmware/include/config.h
+++ software/nightlight-firmware/include/config.h
@@ -10,6 +10,7 @@
 #define touchThreshold 80
 
 // NeoPixel LED Strip Setup
+<<<<<<< HEAD
 #define LED_PIN 1
 #define NUM_PIXELS 8
 
@@ -17,6 +18,15 @@
 #define BRIGHTNESS_STEP 7
 #define MIN_BRIGHTNESS 30
 #define MAX_BRIGHTNESS 255
+=======
+#define LED_PIN PIN_PA4
+#define NUM_PIXELS 15
+
+// Brightness control
+#define BRIGHTNESS_STEP 7 // Amount to change brightness on each long press
+#define MIN_BRIGHTNESS 30  // Minimum brightness (completely off is 0)
+#define MAX_BRIGHTNESS 255 // Maximum brightness
+>>>>>>> parent of ad9992a (messing around during testing)
 
 #endif
 
